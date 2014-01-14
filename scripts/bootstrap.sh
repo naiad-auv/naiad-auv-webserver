9eea9b8e8a31d71416634dfbf71f3f4c5b026d8b
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2014-01-14 11:42:57 +0100

    Updated bootstrap script and added .gitignore file.
    The bootstap script will now properly change the repository to the Swedish
    SUNET provider and upgrade all pre-installed packages by running dist-upgrade.
    
    The .gitignore now ignores ".vagrant".

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
+
+#############################################
+# Basic Ubuntu updating and upgrading
+#############################################
+
 ###
 # Changing to Ubuntu repository closer to home
 # and upgrading to the latest packages.
 ORIG_REPO="http://us.archive.ubuntu.com/ubuntu/"
 NEW_REPO="http://ftp.sunet.se/pub/os/Linux/distributions/ubuntu/ubuntu/"
-sudo sed -i.orig \"s|${ORIG_REPO}|${NEW_REPO}|g\" /etc/apt/sources.list
+sudo sed -i.orig "s|${ORIG_REPO}|${NEW_REPO}|g" /etc/apt/sources.list
 
+###
+# Update package list from new repository
+###
 sudo apt-get update
 
+###
+# Upgrade all packages to the latest version with dist-upgrade
+# The longer more obscure command is to prevent all upgrades from
+# asking any questions and waiting for response. Especially to prevent
+# the upgrade of the package "grub-pc" from displaying the interactive
+# menu asking for where to install the bootloader which will in worst
+# case freeze your VM.
+###
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o \
     Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" \
     dist-upgrade
+
+#############################################
+# Install wanted packages
+#############################################
