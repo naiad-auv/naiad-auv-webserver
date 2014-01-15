ad3aedc0fa631736180fea20af955a9b14594223
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2014-01-15 09:40:49 +0100

    Updated basic configuration with puppet, now configuring a default user.
    Added user{} settings to the nodes manifest.
    Added build-essential package in bootstrap script so "make" command is
    available when building libshadow gem.
    Added libshadow gem when installing puppet so puppet can manage user passwords.

--- puppet/manifests/nodes.pp
+++ puppet/manifests/nodes.pp
@@ -1,3 +1,10 @@
 node default {
-
+    user { 'christoffer':
+        ensure => 'present',
+        groups => ['sudo'],
+        home => '/home/christoffer',
+        managehome => true,
+        password => '$6$b6HwkAtzctT.$6tM.nVrXzsficTolxfzSy/2BeuMKSAGyhWOnx1tf.eb.7pdkM3Qb6Pe11OsHOkEtbnUilT8ln9OBfUzFoZd/m1',
+        shell => '/bin/bash',
+    }
 }
--- scripts/install-puppet.sh
+++ scripts/install-puppet.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+
+#############################################
+# Installation of puppet from gem repo
+# instead of the Ubuntu repository as it is
+# outdated for Ubuntu 12.04.4 LTS.
+#############################################
 /opt/vagrant_ruby/bin/gem install puppet --no-ri --no-rdoc
+/opt/vagrant_ruby/bin/gem install libshadow --no-ri --no-rdoc
