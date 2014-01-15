f468c932c263c9d0ada532ad1adcaefa04ff212a
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2014-01-15 07:20:41 +0100

    Fixed some comments in the bootstrap script.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-
 #############################################
 # Basic Ubuntu updating and upgrading
 #############################################
 
 ###
-# Changing to Ubuntu repository closer to home
-# and upgrading to the latest packages.
+# Changing to a Ubuntu repository closer to home
+###
 ORIG_REPO="http://us.archive.ubuntu.com/ubuntu/"
 NEW_REPO="http://ftp.sunet.se/pub/os/Linux/distributions/ubuntu/ubuntu/"
 sudo sed -i.orig "s|${ORIG_REPO}|${NEW_REPO}|g" /etc/apt/sources.list
