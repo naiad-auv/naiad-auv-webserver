d741375e28673ab0a7503545960e609058b332b9
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2014-01-15 19:02:58 +0100

    Moved user settings to its own puppet module.

--- puppet/manifests/nodes.pp
+++ puppet/manifests/nodes.pp
@@ -1,10 +1,3 @@
 node default {
-    user { 'naiad':
-        ensure => 'present',
-        groups => ['sudo'],
-        home => '/home/naiad',
-        managehome => true,
-        password => '$6$xhCZGn6cQKgbSL8i$N8FRkfatK6rPHBfP1qOVspydAmHa1RpZVB9VUutI.64ArSz6AosEveaxnnYmEG0s8us9SgxvSPExH1SPEQ4me1',
-        shell => '/bin/bash',
-    }
+    include users
 }
---
+++ puppet/modules/users/manifests/init.pp
@@ -0,0 +1,19 @@
+class users {
+    user { 'naiad':
+        ensure => 'present',
+        groups => ['sudo'],
+        home => '/home/naiad',
+        managehome => true,
+        password => '$6$xhCZGn6cQKgbSL8i$N8FRkfatK6rPHBfP1qOVspydAmHa1RpZVB9VUutI.64ArSz6AosEveaxnnYmEG0s8us9SgxvSPExH1SPEQ4me1',
+        shell => '/bin/bash',
+    }
+
+    user { 'christoffer':
+        ensure => 'present',
+        groups => ['sudo'],
+        home => '/home/christoffer',
+        managehome => true,
+        password => '$6$xhCZGn6cQKgbSL8i$N8FRkfatK6rPHBfP1qOVspydAmHa1RpZVB9VUutI.64ArSz6AosEveaxnnYmEG0s8us9SgxvSPExH1SPEQ4me1',
+        shell => '/bin/bash',
+    }
+}
