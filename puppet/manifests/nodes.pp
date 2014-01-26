eca6ca74aa8debb9aeffd7b5b4b342746d21d8b1
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2014-01-26 14:50:44 +0100

    Started to configure Apache2, but it won't start.

--- puppet/manifests/nodes.pp
+++ puppet/manifests/nodes.pp
@@ -1,2 +1,21 @@
 node default {
+
+    file { 'placeholder.conf':
+      path    => '/etc/apache2/conf.d/placeholder.conf',
+      ensure  => present,
+      mode    => 0640,
+      content => "",
+    }
+
+
+    class { 'apache':
+        default_mods        => false,
+        default_confd_files => false,
+    }
+
+    apache::vhost { 'naiad.localhost.dev':
+        port => '80',
+        docroot => '/var/www/naiad.se',
+    }
+
 }
